b542ee7604a50ab4059d869b9705e2545198ea35
Weiyi Chen <weiyic@usc.edu>
2017-01-17 16:45:15 -0800

    initial commit with an example

---
+++ chen_lab1.m
@@ -0,0 +1,28 @@
+% EE301 Lab1, Due 01/20/2017
+%
+% Wayne Weiyi Chen, USCID xxxx-xxxx-xx
+%
+% Make any additional comments/instructions you want the grader/TA to know:
+% This is an example of EE301_20171_Nayak lab submission, please also read
+% the README_for_lab_submission.pdf in the folder.
+
+clear; clc; close all;
+
+%% Q1
+display('------Q1------');
+
+% insert your code for Q1 here
+display('hello world'); % example
+
+% end of your code for Q1
+
+pause;
+%% Q2
+display('------Q2------');
+
+% insert your code for Q2 here
+
+% end of your code for Q2
+
+pause;
+%% Q3
